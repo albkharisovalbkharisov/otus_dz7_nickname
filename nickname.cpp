9f17ba4281bcf4e43dc64152cfdb075e03f238f6
Albert Kharisov <ah@bright-box.com>
2019-03-19 20:29:26 +0300

    first

---
+++ nickname.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <set>
+
+class node
+{
+	std::string data;
+	bool is_end;
+	std::set<node> nexts;
+
+	operator 
+
+public:
+	void add(const std::string &n)
+	{
+		;
+	}
+}
+
+int main(int argc, char ** argv)
+{
+	for(std::string line; std::getline(std::cin, line); ) {
+		b.parse_line(line);
+	}
+
+	return 0;
+}
+
