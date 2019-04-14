031668a317fb5aa04f6c099cff7d662d9b9dd24f
Albert Kharisov <ah@bright-box.com>
2019-04-15 00:13:03 +0300

    ef

--- nickname.cpp
+++ nickname.cpp
@@ -10,77 +10,16 @@ class node
 	bool is_end;
 	std::unique_ptr<std::set<node>> nexts;
 
-//	node& operator[](const std::string &index) const { return nexts[index]; }
-//	node& operator[](const char *index) const { return (*this)[std::string(index)]; }
-
-	/*
-	auto insert(const char *str) const {
-		if (0 == data.size())
-			return *this;
-		auto pos = data.find_first_not_of(str);
-		if (pos != std::string::npos)
-			return *this;
-		if (nexts.empty())
-			nexts.emplace("");
-		for (auto &a : nexts) {
-			auto b = a[str + pos];
-			if (b != nullptr)
-				return;
-		}
-		return nullptr;
-	}
-*/
-
-
-	void insert(size_t pos, const char *str)
-	{
-		if ((pos == data.size()) && (strlen(str) == 0)) {
-			is_end = true;
-			return;
-		}
-
-		if (pos == data.size()) {
-			node n{str};
-
-
-
-
-
-
-		} else {
-			std::string p{data.c_str, pos};
-			data.erase(pos);
-			n.nexts = nexts;
-			nexts = new decltype(*nexts);
-			nexts.insert();
-		}
-
-	}
-
-
-	auto add(const char *str) const
-	{
-		auto pos = data.find_first_not_of(str);
-		if (pos != std::string::npos) {
-			insert(pos, str + pos);
-			return this;
-		}
-
-		for (const auto &a : *nexts) {
-			if (a.data[0] == str[pos])
-				return a.add(str + pos);
-		}
-		return this;
-	}
-
-
+	void node::insert(std::string::size_type pos, const char *str);
 
 public:
 	node(const std::string &s) : data(s), is_end(false) {}
 	node(void) = default;
 	bool operator<(const node& n) const { return data < n.data; }
 	bool operator!=(const node& n) const { return data != n.data; }
+	void node::add(const char *str);
 
+#if 0
 	void add(const std::string &name)
 	{
 		if (nexts.empty()) {
@@ -101,7 +40,58 @@ public:
 			}
 		}
 	}
+#endif	// 0
 };
+
+
+void node::insert(std::string::size_type pos, const char *str)
+{
+	std::cout << "insert: \"" << data << "\", pos = " << pos << ", str = " << str << std::endl;
+	if (pos == std::string::npos) {
+		
+	} else if ((pos == data.size()) && (strlen(str) == 0)) {
+		std::cout << "case 1" << std::endl;
+		is_end = true;
+	} else if (pos == data.size()) {
+		std::cout << "case 2" << std::endl;
+		nexts->emplace(str);
+	} else {
+		std::cout << "case 3" << std::endl;
+		node tail{std::string{data.c_str, pos}};
+		data.erase(pos);
+
+		tail.nexts = nexts;		// move unique_ptr
+		nexts = std::make_unique();	// new decltype(*nexts);
+		tail.is_end = true;
+		is_end = false;
+		nexts->emplace(str);
+		nexts->insert(std::move(tail));		// nexts->emplace(tail);
+	}
+
+}
+
+
+void node::add(const char *str)
+{
+	std::cout << "add: \"" << str << "\"" << std::endl;
+	auto pos = data.find_first_not_of(str);
+	if (pos != std::string::npos) {
+		insert(pos, str + pos);
+		return;
+	}
+
+	str += data.size();
+
+	for (const auto &a : *nexts) {
+		if (a.data[0] == str[0]) {	// quick way to peep is this string are candidate to match string
+			a.add(str);
+			return;		// all strings starts from different letters, so stop searching
+		}
+	}
+	insert(pos, str);
+}
+
+
 
 int main(int argc, char ** argv)
 {
