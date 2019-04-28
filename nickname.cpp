211e008aac6226044d36aab21d4fe63576e1b6ce
Karkusha <albkharisov@gmail.com>
2019-04-29 02:20:16 +0300

    seems OK, but have bug

--- nickname.cpp
+++ nickname.cpp
@@ -6,6 +6,9 @@
 #include <memory>
 #include <forward_list>
 #include <string.h>
+#include <list>
+#include <functional>
+
 
 class node
 {
@@ -16,38 +19,49 @@ class node
 	void insert(std::string::size_type pos, const char *str);
 
 public:
-	node(const std::string &s)	: data(s), is_end(false), nexts(std::make_unique<std::vector<node>>()) {}
-	node(const char *s)		: data(s), is_end(false), nexts(std::make_unique<std::vector<node>>()) {}
+	node(const std::string &s)	: data(s), is_end(true), nexts(std::make_unique<std::vector<node>>()) {}
+	node(const char *s)		: data(s), is_end(true), nexts(std::make_unique<std::vector<node>>()) {}
 	node(void)			: data(),  is_end(false), nexts(std::make_unique<std::vector<node>>()) {}
 	bool operator<(const node& n) const { return data < n.data; }
 	bool operator!=(const node& n) const { return data != n.data; }
 	void add(const char *str);
 	void show_all(void);
+	void help(void);
 };
 
 
 void node::insert(std::string::size_type pos, const char *str)
 {
-	std::cout << "insert: \"" << data << "\", pos = " << pos << ", str = " << str << std::endl;
+	std::cout << "insert: \"" << data << "\", pos = " << pos << ", str = \"" << str << "\"" << std::endl;
 	if (pos == std::string::npos) {
-		if (strlen(str) == 0) {
+		if (data.empty() && !is_end) {
+			std::cout << "case 0" << std::endl;
+			data = str;
+			is_end = true;
+		} else if (strlen(str) == 0) {
 			std::cout << "case 1" << std::endl;
 			is_end = true;
 		} else {
 			std::cout << "case 2" << std::endl;
 			nexts->emplace_back(str);
+			is_end = true;
 		}
 	} else {
 		std::cout << "case 3" << std::endl;
-		node tail{std::string{data.c_str(), pos}};
+		node tail{std::string{data.c_str(), pos, std::string::npos}};
 		data.erase(pos);
 
 		tail.nexts = std::move(nexts);
 		nexts = std::make_unique<std::vector<node>>();
 		tail.is_end = is_end;
-		is_end = false;
-		nexts->emplace_back(str);
 		nexts->emplace_back(std::move(tail));
+		if (strlen(str) > 0) {
+			nexts->emplace_back(str);
+			is_end = false;
+		}
+		else {
+			is_end = true;
+		}
 	}
 }
 
@@ -79,20 +93,47 @@ void node::add(const char *str)
 
 void node::show_all(void)
 {
-//	std::forward_list<std::string *> fls;
+#if 1
 	std::list<std::string *> fls;
-//	auto head = fls.before_begin();
-	
-	auto f = [&](node & n)
-	{
+
+	std::function<void(node &)> f = [&fls, &f, this] (node &n) {
+		std::cout << "enter" << std::endl;
+		if (is_end) {
+			std::cout << "is_end true" << std::endl;
+			for (auto a : fls)
+				std::cout << *a << '\n';
+			std::cout << n.data << std::endl;
+		}
+		if (n.nexts->empty())
+			return;
+
 		fls.push_back(&n.data);
-		for (auto & a : *nexts) {
+		for (auto& a : *(n.nexts)) {
 			f(a);
 		}
-		fls.erase(fls.rbegin());
+		fls.pop_back();
+		std::cout << "exit" << std::endl;
 	};
-	
-	f(it, *this);
+
+	f(*this);
+#endif	// 0
+}
+
+void node::help(void)
+{
+	static int tabs = 0;
+	for (int i = 0; i < tabs; ++i) std::cout << "\t";
+	std::cout << "data: \"" << data << "\"" << std::endl;
+	for (int i = 0; i < tabs; ++i) std::cout << "\t";
+	std::cout << "is_end: " << is_end << std::endl;
+
+	std::cout << "{" << std::endl;
+	++tabs;
+	for (auto &a : *nexts) {
+		a.help();
+	}
+	--tabs;
+	std::cout << "}" << std::endl;
 }
 
 int main(int argc, char ** argv)
@@ -101,6 +142,10 @@ int main(int argc, char ** argv)
 	node head{};
 	for(std::string line; std::getline(std::cin, line); ) {
 		head.add(line.c_str());
+		std::cout << "============= tree ===========" << std::endl;
+//		head.show_all();
+		head.help();
+		std::cout << "=======================" << std::endl;
 	}
 
 	return 0;
